223c02311f9499cccae70eee9a8da751e13e70d6
elianbenjamin <elianbenjamin30@gmail.com>
2024-02-07 14:11:43 -0300

    update home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-import styles from './Home.module.css'
-
-
-
+import { useState} from "react";
+import {scroller} from "react-scroll"
+import styles from "./Home.module.css";
+import whatsApp from "../../assets/image/3225179_app_logo_media_popular_social_icon.png";
+import arrow from "../../assets/image/arrooow.png"
+import Service from "../../views/Service/Service";
 
 const Home = () => {
-  return (
-    <div className={styles.container}
-    
-    style={{
-      background: `url(https://imgs.search.brave.com/smrbS6-_vAsNTRNi2of631qBFqe1zI66Mfh_9K0sG9E/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9mb3Jj/ZXJlcy5jb20uYXIv/d3AtY29udGVudC91/cGxvYWRzLzIwMjEv/MDYvcGljYWRvMi0x/MzAweDg2Ny5qcGc)`,
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      
-    }}>
-      
-      <div>
-
-      <h2 className={styles.text}>- Desde 2006 -</h2>
-      <hr />
-      <h1 className={styles.text}>AGROFORRAJES FERREYRA</h1>
-      <hr />
-      <h2 className={styles.text}>Calidad Asegurada</h2>
-
-      </div>
 
   
-     
-    
-    </div>
-  )
-}
 
-export default Home
+  const [whatsappNumber] = useState(3329561698);
+
+  const handleHireMeClick = () => {
+    window.open(`https://wa.me/${whatsappNumber}`, "_blank");
+  };
+
+  const handleScrollToNext = () => {
+    scroller.scrollTo('services', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h2 className={styles.text}>- Desde 2006 -</h2>
+        <hr />
+        <h1 className={styles.text}>AGROFORRAJES FERREYRA</h1>
+        <hr />
+        <h2 className={styles.text}>Calidad Asegurada</h2>
+      </div>
+
+      <div className={styles.btnwhat}>
+        <img
+          onClick={handleHireMeClick}
+          className={styles.img}
+          src={whatsApp}
+          alt=""
+        />
+      </div>
+
+      <div>
+        <button onClick={handleScrollToNext}>
+          <img
+            src={arrow}
+            alt="Flecha abajo"
+            style={{ width: "40px", height: "40px" }}
+          />
+        </button>
+
+      </div>
+     
+
+
+    </div>
+  );
+};
+
+export default Home;
